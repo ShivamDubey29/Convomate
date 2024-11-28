8aad023db889dc56c72516687ddba27be94339d0
Shivam C. Dubey <shivamc.dubey@Shivams-MacBook-Air.local>
2024-11-28 12:28:15 +0530

    first commit

--- script.js
+++ script.js
@@ -3,12 +3,44 @@ const chatList = document.querySelector(".chat-list");
 
 let userMessage = null;
 
+//API Configuration
+const API_KEY = "AIzaSyBiQNqr17DoMqIsgu9JvEbcHIOul1ln1-4";
+
+const API_URl =`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
+
 //Create a new message element and return it 
-const createMessageEkement = (content, className ) => {
+const createMessageEkement = (content, ...classes ) => {
     const div = document.createElement("div");
-    div.classList.add("message", className);
+    div.classList.add("message", ...classes);
     div.innerHTML = content;
     return div;
+}
+
+//Fetch response from the API based on user message 
+const generateAPIResponse = async (incomingMessageDiv) =>{
+    const textElement = incomingMessageDiv.querySelector(".text")//Get text element
+    //send a POST request to the API with the user's message
+    try{
+        const response = await fetch(API_URl,{
+            method : "POST",
+            headers : {"Content-Type": "application/json"},
+            body : JSON.stringify({
+                contents:[{
+                    role : "user",
+                    parts: [{text:userMessage}]
+                }]
+            })
+        });
+
+        const data = await response.json();
+
+        const apiResponse = data?.candidates[0].content.parts[0].text;
+        textElement.innerText = apiResponse;
+    }catch(error){
+        console.log(error);
+    } finally{
+        incomingMessageDiv.classList.remove("loading");
+    }
 }
 
 //Show a loading animation while waiting for the API response 
@@ -23,11 +55,12 @@ const showLoadingAnimation = () =>{
         <div class="loading-bar"></div>
     </div>
 </div>
-<span class="icon material-symbols-rounded">content_copy</span>
-`;
+<span class="icon material-symbols-rounded">content_copy</span>`;
 
 const incomingMessageDiv = createMessageEkement(html,"incoming", "loading");
 chatList.appendChild(incomingMessageDiv);
+
+generateAPIResponse(incomingMessageDiv);
 }
 
 //Handle sendong outgoing chat messages 
